cf7b2080728b36543073fc2ed6db7e33f27f2da5
Joel Myers <joeldmyers@gmail.com>
2022-06-12 18:07:02 -0400

    bfs and dfs

--- binary-search-tree/binary-search-tree.ts
+++ binary-search-tree/binary-search-tree.ts
@@ -68,4 +68,78 @@ class BinarySearchTree {
 
     return currentNode;
   }
+
+  BFS() {
+    let dataFound = [];
+    let queue = [];
+    let currentNode = this.root;
+
+    queue.push(currentNode);
+
+    while (queue.length) {
+      currentNode = queue.shift() || null;
+      dataFound.push(currentNode?.value);
+
+      if (currentNode?.left) {
+        queue.push(currentNode.left);
+      }
+
+      if (currentNode?.right) {
+        queue.push(currentNode.left);
+      }
+    }
+
+    return dataFound;
+  }
+
+  // top down and to the left first, then right.
+  DFSPreOrder() {
+    let data: BSTNode[] = [];
+    let current = this.root;
+    if (current === null) return;
+
+    const traverse = (node: BSTNode) => {
+      // **** PRE ORDER DATA PUSH ****
+      data.push(node);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+
+    traverse(current);
+    return data;
+  }
+
+  // starts at bottom left and goes up - root is last thing visited.
+  DFSPostOrder() {
+    let data: BSTNode[] = [];
+    let current = this.root;
+    if (current === null) return;
+
+    const traverse = (node: BSTNode) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      // ***** POST ORDER DATA PUSH ****
+      data.push(node);
+    };
+
+    traverse(current);
+    return data;
+  }
+
+  // starts at bottom left and basically goes from left to right.
+  DFSInOrder() {
+    let data: BSTNode[] = [];
+    let current = this.root;
+    if (current === null) return;
+
+    const traverse = (node: BSTNode) => {
+      if (node.left) traverse(node.left);
+      // ***** IN ORDER DATA PUSH ****
+      data.push(node);
+      if (node.right) traverse(node.right);
+    };
+
+    traverse(current);
+    return data;
+  }
 }
