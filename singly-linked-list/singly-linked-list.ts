375102e74bad18dca01b690fa982aea853bb0b07
Joel Myers <joeldmyers@gmail.com>
2022-06-12 09:08:04 -0400

    correction

--- singly-linked-list/singly-linked-list.ts
+++ singly-linked-list/singly-linked-list.ts
@@ -74,10 +74,11 @@ class SinglyLinkedList {
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+      this.length++;
+      return this;
     }
-    newNode.next = this.head;
-    this.head = newNode;
-    this.length++;
-    return this;
   }
 }
