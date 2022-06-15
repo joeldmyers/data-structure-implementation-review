81e91eb0b53547b4343ba84f80c98e4c2bdd8dfb
Joel Myers <joeldmyers@gmail.com>
2022-06-14 22:17:13 -0400

    extract max

--- binary-heap/binary-heap.ts
+++ binary-heap/binary-heap.ts
@@ -42,4 +42,45 @@ class MaxBinaryHeap {
 
     // take node and compare to parent. if > parent, swap.
   }
+
+  extractMax() {
+    // take the first value in it.
+    // replace with the most recently added (item at the end)
+    // sink this down, swapping with larger of two children until it's in the right place.
+    const max = this.values[0];
+
+    const lastItem = this.values.pop();
+    if (lastItem) {
+      this.values[0] = lastItem;
+    }
+
+    let done = false;
+    let indexOfCurrentItem = 0;
+
+    while (!done) {
+      let leftChildIndex = indexOfCurrentItem * 2 + 1;
+      let rightChildIndex = indexOfCurrentItem * 2 + 2;
+
+      let leftChild, rightChild;
+
+      leftChild = this.values[leftChildIndex] || Number.NEGATIVE_INFINITY;
+
+      rightChild = this.values[rightChildIndex] || Number.NEGATIVE_INFINITY;
+
+      let largerChild = rightChild > leftChild ? rightChild : leftChild;
+      let winningChildIndex =
+        rightChild > leftChild ? rightChildIndex : leftChildIndex;
+
+      if (this.values[indexOfCurrentItem] < largerChild) {
+        const temp = this.values[indexOfCurrentItem];
+        this.values[indexOfCurrentItem] = largerChild;
+        this.values[winningChildIndex] = temp;
+        indexOfCurrentItem = winningChildIndex;
+      } else {
+        done = true;
+      }
+    }
+
+    return max;
+  }
 }
