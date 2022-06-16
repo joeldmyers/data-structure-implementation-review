b9b87e6d942b0e0fc921f3d3aa993f6f3723b777
Joel Myers <joeldmyers@gmail.com>
2022-06-16 19:30:11 -0400

    adding limit

--- hash-table/hash-table.ts
+++ hash-table/hash-table.ts
@@ -3,11 +3,13 @@ class HashTable {
   // need to convert keys into valid array indices
   // what makes a good hash - fast, not clustered (distributed evenly), and deterministic (non-random)
 
-  hash(key: string) {
-    return key
-      .split("")
-      .map((val) => val.charCodeAt(0))
-      .reduce((prev: number, cur: number) => prev + cur, 0);
+  hash(key: string, arrayLength = 10) {
+    return (
+      key
+        .split("")
+        .map((val) => val.charCodeAt(0))
+        .reduce((prev: number, cur: number) => prev + cur, 0) % arrayLength
+    );
   }
 }
 
