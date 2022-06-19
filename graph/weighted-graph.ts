6b75b5c0df72a8ef86734c83f1cf717829507a2b
Joel Myers <joeldmyers@gmail.com>
2022-06-19 14:55:07 -0400

    wip dijkstras

--- graph/weighted-graph.ts
+++ graph/weighted-graph.ts
@@ -1,4 +1,7 @@
+const util = require("util");
+
 type GraphNode = { node: string; weight: number };
+type SimpleQueueNode = { val: string; priority: number };
 
 class WeightedGraph {
   adjacencyList: Record<string, GraphNode[]>;
@@ -15,4 +18,49 @@ class WeightedGraph {
     this.adjacencyList[vertex1].push({ node: vertex2, weight });
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
   }
+
+  findShortestPath(startingVertex: string, endingVertex: string) {}
 }
+
+class SimplePriorityQueue {
+  values: SimpleQueueNode[];
+
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val: string, priority: number) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+
+  dequeue() {
+    return this.values.shift();
+  }
+
+  sort() {
+    // ascending order, so lowest priority first.
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
+const weightedGraph = new WeightedGraph();
+
+weightedGraph.addVertex("A");
+weightedGraph.addVertex("B");
+weightedGraph.addVertex("C");
+weightedGraph.addVertex("D");
+weightedGraph.addVertex("E");
+weightedGraph.addVertex("F");
+
+weightedGraph.addEdge("A", "B", 4);
+weightedGraph.addEdge("A", "C", 2);
+weightedGraph.addEdge("B", "E", 3);
+weightedGraph.addEdge("C", "F", 4);
+weightedGraph.addEdge("D", "E", 3);
+weightedGraph.addEdge("D", "F", 1);
+weightedGraph.addEdge("E", "F", 1);
+
+console.log(
+  util.inspect(weightedGraph, { showHidden: false, depth: null, colors: true })
+);
