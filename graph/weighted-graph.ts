36c735887bd4da07bc571f26f57446c95813f050
Joel Myers <joeldmyers@gmail.com>
2022-06-19 10:59:41 -0400

    adding weighted graph class

---
+++ graph/weighted-graph.ts
@@ -0,0 +1,18 @@
+type GraphNode = { node: string; weight: number };
+
+class WeightedGraph {
+  adjacencyList: Record<string, GraphNode[]>;
+
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex: string) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1: string, vertex2: string, weight: number) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+}
