4391496ee4ed6e7f2cf780a4a16024209d4dd4d0
Joel Myers <joeldmyers@gmail.com>
2022-09-12 20:52:14 -0400

    updated binary heap implementation

--- binary-heap/binary-heap.ts
+++ binary-heap/binary-heap.ts
@@ -4,83 +4,82 @@
 
 // heap is always as compact as possible. as full as can be. left children are always filled out first.
 
-class MaxBinaryHeap {
+// Note: I rewrote this after looking at this solution: https://reginafurness.medium.com/implementing-a-max-heap-in-javascript-b3e2f788390c
+// While there are some little cleanup things, one thing I like about her solution is that
+// it really breaks up some of these things into helper methods - parent, leftChild, rightChild, swap, etc.
+// it makes it more readable / intuitive.
+
+class MaxHeap {
   values: number[];
 
   constructor() {
     this.values = [];
   }
 
-  // to insert: add to the end and bubble up.
+  parent(index: number) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  leftChild(index: number) {
+    return index * 2 + 1;
+  }
+
+  rightChild(index: number) {
+    return index * 2 + 2;
+  }
+
+  swap(idx1: number, idx2: number) {
+    [this.values[idx1], this.values[idx2]] = [
+      this.values[idx2],
+      this.values[idx1],
+    ];
+  }
 
   insert(value: number) {
-    // add to end.
     this.values.push(value);
+    this.bubbleUp(this.values.length - 1);
+  }
 
-    let currentNodeIndex = this.values.length - 1;
+  bubbleUp(idx: number) {
+    let currentIndex = idx;
+    let parentIndex = this.parent(currentIndex);
 
-    let done = false;
-
-    while (!done) {
-      if (this.values.length === 1) {
-        done = true;
-      } else {
-        const currentParentNodeIndex = Math.floor((currentNodeIndex - 1) / 2);
-        if (
-          this.values[currentNodeIndex] > this.values[currentParentNodeIndex]
-        ) {
-          // swap
-          const temp = this.values[currentNodeIndex];
-          this.values[currentNodeIndex] = this.values[currentParentNodeIndex];
-          this.values[currentParentNodeIndex] = temp;
-          currentNodeIndex = currentParentNodeIndex;
-        } else {
-          done = true;
-        }
-      }
+    while (
+      currentIndex > 0 &&
+      this.values[currentIndex] > this.values[parentIndex]
+    ) {
+      this.swap(currentIndex, parentIndex);
+      currentIndex = parentIndex;
+      parentIndex = this.parent(parentIndex);
     }
-
-    // take node and compare to parent. if > parent, swap.
   }
 
   extractMax() {
-    // take the first value in it.
-    // replace with the most recently added (item at the end)
-    // sink this down, swapping with larger of two children until it's in the right place.
+    if (this.values.length < 1) return;
+
     const max = this.values[0];
+    const end = this.values.pop();
 
-    const lastItem = this.values.pop();
-    if (lastItem) {
-      this.values[0] = lastItem;
-    }
+    this.values[0] = end || 0;
 
-    let done = false;
-    let indexOfCurrentItem = 0;
-
-    while (!done) {
-      let leftChildIndex = indexOfCurrentItem * 2 + 1;
-      let rightChildIndex = indexOfCurrentItem * 2 + 2;
-
-      let leftChild, rightChild;
-
-      leftChild = this.values[leftChildIndex] || Number.NEGATIVE_INFINITY;
-
-      rightChild = this.values[rightChildIndex] || Number.NEGATIVE_INFINITY;
-
-      let largerChild = rightChild > leftChild ? rightChild : leftChild;
-      let winningChildIndex =
-        rightChild > leftChild ? rightChildIndex : leftChildIndex;
-
-      if (this.values[indexOfCurrentItem] < largerChild) {
-        const temp = this.values[indexOfCurrentItem];
-        this.values[indexOfCurrentItem] = largerChild;
-        this.values[winningChildIndex] = temp;
-        indexOfCurrentItem = winningChildIndex;
-      } else {
-        done = true;
-      }
-    }
+    this.sinkDown(0);
 
     return max;
+  }
+
+  sinkDown(index: number) {
+    const leftChildIndex = this.leftChild(index);
+    let rightChildIndex = this.rightChild(index);
+
+    let largerChildIndex =
+      this.values[rightChildIndex] > this.values[leftChildIndex]
+        ? rightChildIndex
+        : leftChildIndex;
+
+    if (this.values[index] < this.values[largerChildIndex]) {
+      this.swap(index, largerChildIndex);
+
+      this.sinkDown(largerChildIndex);
+    }
   }
 }
